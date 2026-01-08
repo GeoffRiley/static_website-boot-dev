69da77530013d5653ab97b1a584a04e104d0c7e7
Geoff Riley <geoffr@adaso.com>
2026-01-08 02:00:42 +0000

    git it onto the gitpages

--- src/main.py
+++ src/main.py
@@ -3,14 +3,22 @@ import shutil
 
 from makesite import copy_directory_contents, generate_pages_recursive
 
-def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
+def main(base_path):
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
 
-    copy_directory_contents("static", "public")
+    copy_directory_contents("static", "docs")
 
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
 
-main()
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    else:
+        base_path = "/"
+
+    main(base_path)
 
